f32a7f0fc2523d19c67d7e5a90848a42b363a746
Abgr8 <abbasraza666666@gmail.com>
2025-06-16 08:00:52 +0530

    fixed meaningless errors showing up in errorlens for better dev experience

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,8 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
